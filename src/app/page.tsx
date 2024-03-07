d2997f31a5227a8655b3b954fb394301969d65fe
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-03-07 03:08:58 -0800

    Update page.tsx
    added navbar for site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import ThemeSwitch from "@/components/ThemeSwitch";
+import NavBar from "@/components/nav/NavBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ThemeSwitch />
+      <div className="flex flex-col items-center gap-4">
+        <ThemeSwitch />
+        <NavBar />
+      </div>
       <h1>Coming Soon...</h1>
     </main>
   );
